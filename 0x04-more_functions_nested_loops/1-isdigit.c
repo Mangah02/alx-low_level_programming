b28b4b5d6f757815aa1b2d4527ef8c711cc8d489
Mangah02 <jmanga86@gmail.com>
2022-03-17 14:13:44 +0300

    check if its digit or not

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isdigit - checks whether it a digit
- * @c: character to be checked if its digit 
+ * _isdigit - checks for digithood
+ * @c: character to checked for digit membership
  *
  * Return: 1 if digit, 0 if not digit
  */
