a91a2e721bf945cc8c4b2d5c6baf49459d392710
Joel Manga <jmanga86@gmail.com>
2022-03-23 14:53:41 +0300

    function that prints an integer.

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *void print_number. 
+ *@n number to print
+ *
+ */
+
+void print_number(int n)
+{
+	int a;
+	for (a = 0 ; n[a] != '\0' ; a++);
+		if (n == a)
+	{
+		_putchar(n);
+	{
+}
+      
