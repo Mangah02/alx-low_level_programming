571cc2b4ac82d6ff6bffa6bb644afe5ac5a5edab
Joel Manga <jmanga86@gmail.com>
2022-03-28 14:50:48 +0300

    searche a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,27 @@
 #include "main.h"
+#define NULL 0
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: string to search
- * @accept: set of bytes to search for
- * Description: retruns first occurrence in a string
- * Return: pointer to first instance of string
-**/
+ * @s: string
+ * @accept: chars to compare
+ * Return: pointer from the initial segment of match
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (accept[j] == s[i])
+			if (*(s + i) == *(accept + j))
 			{
-				return (&s[i]);
+				return (s + i);
 			}
 		}
 	}
+
 	return (NULL);
 }
