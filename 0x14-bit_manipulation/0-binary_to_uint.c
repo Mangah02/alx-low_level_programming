31863f2350abaa1b98f6183fa3c55b7910c7a1c4
Joel Manga <jmanga86@gmail.com>
2022-04-28 15:03:45 +0300

    convert binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,61 +1,63 @@
-  #include "main.h"
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _strlen - returns length of string (modified)
- * @s: string (const)
- * Return: length of string
+ * power - returns 2 ^ y
+ * @c: only calculate if c != 0
+ * @y: exponent value
+ * Return: 2 ^ y or 0 if fail
  */
-
-int _strlen(const char *s)
+int power(char c, int y)
 {
-	int len = 0;
+	if (c == '0')
+		return (0);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (2);
+	return (2 * power(c, y - 1));
+}
+/**
+ * binary - converts a string binary number into a unsigned int
+ * @b: binary string
+ * @num: pointer to number where value is stored
+ * Return: decimal as unsigned int
+ */
+unsigned int binary(const char *b, unsigned int *num)
+{
+	int index;
 
-	while (s[len] != '\0')
+	if (b[1] == '\0')
 	{
-		len++;
+		*num = b[0] - '0';
+		return (1);
 	}
-
-	return (len);
+	index = binary(b + 1, num);
+	*num +=  power(b[0], index);
+	return (index + 1);
 }
 
 /**
- * power - exponents
- * @base: base
- * @exp: exponent
- * Return: result (int)
+ * binary_to_uint - converts a string binary number into a unsigned int
+ * @b: binary string
+ * Return: decimal as unsigned int
  */
-
-int power(int base, int exp)
-{
-	int i, num;
-
-	num = 1;
-	for (i = 0; i < exp; ++i)
-		num *= base;
-
-	return (num);
-}
-
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary
- * Return: unsigned int
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	int length, i;
+	unsigned int num, i;
 
-	sum = 0;
-	if (b == NULL)
-		return (sum);
-	length = _strlen(b);
-	for (i = length - 1; i >= 0; i--)
+	num = 0;
+
+	if (b == NULL || b[0] == '\0')
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
-		sum += (b[i] - '0') * power(2, length - i - 1);
 	}
-	return (sum);
+
+	binary(b, &num);
+	return (num);
 }
