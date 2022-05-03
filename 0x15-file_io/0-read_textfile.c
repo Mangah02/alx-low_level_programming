7226dc9c9833d43a65458b49b6b05bac10ab5cee
Joel Manga <jmanga86@gmail.com>
2022-05-03 19:53:20 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,47 @@
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 /**
- * read_textfile - prints from file
- * @filename: files name to read from
- * @letters: ma amount of needed space
- * Return: size of buf
- **/
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to read
+ *
+ * Return: actual number of letters read, 0 if end of file
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int re;
+	int file;
+	int length, wrotechars;
 	char *buf;
-	ssize_t j; /*read*/
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
-	re = open(filename, O_RDONLY);
-
-	if (re == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * (letters));
 	if (buf == NULL)
 		return (0);
-	j = read(re, buf, letters);
-	if (j == -1)
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
 		return (0);
-	buf[j] = '\0';
-	close(re);
-	write(STDOUT_FILENO, buf, j);
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
+		return (0);
+	}
+
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
 	free(buf);
-	return (j);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
 }
