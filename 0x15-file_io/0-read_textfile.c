ed837c49b8f2d2b2cfdc5ee63cc1d4c2c2fd7b64
Joel Manga <jmanga86@gmail.com>
2022-05-04 15:47:24 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,37 @@
 #include "main.h"
+
 /**
- * read_textfile - function reads a text file and prints
- * it to the POSIX standard output.
- * @filename: char
- * @letters: list
- * Return: size of the list
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int open_file, write_read;
-ssize_t sizeoflist;
-char *buffer = malloc(letters);
-if (buffer == NULL)
-{
-return (0);
-}
-if (filename == NULL)
-return (0);
-open_file = open(filename, O_RDONLY, 0600);
-if (open_file == -1)
-{
-return (0);
-}
-sizeoflist = read(open_file, buffer, letters);
-buffer[letters] = '\0';
-close(open_file);
-write_read = write(STDOUT_FILENO, buffer, sizeoflist);
-if (write_read == -1)
-{
-return (0);
-}
-return (sizeoflist);
+	int fd;
+	ssize_t i, j;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	i = read(fd, buf, letters);
+	j = write(STDOUT_FILENO, buf, i);
+
+	close(fd);
+
+	free(buf);
+
+	return (j);
 }
